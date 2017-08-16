0a2dd8b410363e91a1a3d9da415d7d3387807def
zokkin89 <jsj60411@naver.com>
2017-08-16 18:08:46 +0900

    웹카페 서적 마크업 및 디자인

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,10 +1,21 @@
 $(function() {
-    $('.btn-menubar').on('click', function() {
-        $(this).parent().siblings()
-            .toggleClass('main-menu-act');
+    var button = $('.btn-menubar');
+    var button_span = $('.btn-menubar, .menubar');
+    var container = $('.menubar-container');
+    var main_menu = $('.main-menu');
+    var last_menu = $('.main-menu li:last-child a');
+
+    button_span.on('click', function() {
+        container.toggleClass('menubar-act');
+        main_menu.toggleClass('main-menu-act');
+        if (container.hasClass('menubar-act')) {
+            button.text('메인 메뉴 닫기');
+        } else {
+            button.text('메인 메뉴 보기');
+        }
     });
-    $('.menubar').on('click', function() {
-        $(this).parent().siblings()
-            .toggleClass('main-menu-act');
+    last_menu.on('focusout', function() {
+        container.toggleClass('menubar-act');
+        main_menu.toggleClass('main-menu-act');
     });
 });
